de1a15c5e5e82cb8ca74e412e3d861b4172edb81
Gengry <545620701@qq.com>
2017-12-25 17:19:05 +0800

    配置fastjson

--- src/main/java/com/zhonglianxs/erp/cpw/config/WebAppConfigurer.java
+++ src/main/java/com/zhonglianxs/erp/cpw/config/WebAppConfigurer.java
@@ -1,9 +1,15 @@
 package com.zhonglianxs.erp.cpw.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.zhonglianxs.erp.cpw.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class WebAppConfigurer extends  WebMvcConfigurerAdapter {
@@ -13,4 +19,19 @@ public class WebAppConfigurer extends  WebMvcConfigurerAdapter {
         registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/**").excludePathPatterns("/").excludePathPatterns("/login");
         super.addInterceptors(registry);
     }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+
+        converters.add(fastConverter);
+    }
+
 }
